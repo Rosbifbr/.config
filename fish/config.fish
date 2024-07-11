6a882fab8ff41e02656006ba53712fffd3634b78
Rodrigo Guimarães Ourique <rodrigo.guima.ourique@gmail.com>
2024-07-11 19:23:09 -0300

    ZED and fish updates

--- fish/config.fish
+++ fish/config.fish
@@ -3,5 +3,6 @@ if status is-interactive
 	source ~/Documents/int6-tooling/host_aliases
 	source ~/Documents/helpers/fish_helpers.fish
 	source ~/Documents/int6-tooling/fish_functions
+	set -U fish_user_paths /home/rodrigo/.cargo/bin $fish_user_paths
 	set fish_greeting ''
 end
