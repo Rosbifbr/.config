f554ddf67bb1d61c300b38aeda32c4dd25f14ea7
Rodrigo Ourique <rodrigo.guima.ourique@gmail.com>
2024-07-28 08:30:33 -0300

    fix paths

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,8 @@
 if status is-interactive
 	# Commands to run in interactive sessions can go here
-	source ~/Documents/int6-tooling/host_aliases
-	source ~/Documents/helpers/fish_helpers.fish
-	source ~/Documents/int6-tooling/fish_functions
+	source /home/rodrigo/Documents/int6-tooling/host_aliases
+	source /home/rodrigo/Documents/helpers/fish_helpers.fish
+	source /home/rodrigo/Documents/int6-tooling/fish_functions
 	set -U fish_user_paths /home/rodrigo/.cargo/bin $fish_user_paths
 	set fish_greeting ''
 end
